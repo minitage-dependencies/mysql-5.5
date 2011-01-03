add5ad0ab1074cd90794c8a0d0d5c7fe2af1d9a0
kiorky <kiorky@cryptelium.net>
2011-01-03 23:03:51 +0000

    up

---
+++ hooks.py
@@ -0,0 +1,16 @@
+import os
+
+def h(options, buildout):
+    compile_dir=options["compile-directory"]
+    cwd = os.getcwd()
+    os.chdir(compile_dir)
+    #os.system("cmake %s" % options["configure-options"])
+    #fps = [os.path.join('core', 'sql', 'cmake_install.cmake'),
+    #       'CMakeLists.txt']
+    #for fp in fps:
+    #    f = open(fp).read()
+    #    content = f.replace('/usr/share/postlbs', '%s/share/postlbs' % options['location'])
+    #    open(fp, 'w').write(content)
+    os.system("cmake %s" % options["cmake-options"])
+    os.chdir(cwd)
+
